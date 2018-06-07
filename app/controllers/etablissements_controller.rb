a6af52f9e40ab727e53a28ff4b24baec1e7f6959
Andryhajanirina <andryhaj@gmail.com>
2018-06-07 12:25:20 +0300

    Ajout du module upload fichier

--- app/controllers/etablissements_controller.rb
+++ app/controllers/etablissements_controller.rb
@@ -1,19 +1,39 @@
 class EtablissementsController < ApplicationController
+  before_action :get_id, only: [:show, :update]
+
   def index
+    @etablissements = Etablissement.all
   end
 
-  def show
+  def show   
   end
 
   def new
+    @etablissement = Etablissement.new
   end
 
   def create
+    @etablissement = Etablissement.new(etablissement_params)
+    if @etablissement.save
+      render "index"
+    else
+      render "new"
+    end
   end
 
   def update
   end
 
   def delete
+  end
+
+  private
+
+  def etablissement_params
+    params.require(:etablissement).permit(:nom, :mail, :telephone, :adress, :description, :category, :longitude, :latitude, :dossier_a_fournir)
+  end
+
+  def get_id
+    @etablissement = Etablissement.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_145347) do
+ActiveRecord::Schema.define(version: 2018_06_07_145348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,10 @@ ActiveRecord::Schema.define(version: 2018_06_07_145347) do
     t.float "latitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "dossier_a_fournir_file_name"
+    t.string "dossier_a_fournir_content_type"
+    t.integer "dossier_a_fournir_file_size"
+    t.datetime "dossier_a_fournir_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
