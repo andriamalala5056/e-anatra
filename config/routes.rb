e02060fd8c8dcf1e9b3022c7c3fe0b9d92c90bb6
Mino <mino.rakoto94@gmail.com>
2018-06-07 09:01:54 +0300

    Création du model et controller et vue du etablissement

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'etablissements/index'
+  get 'etablissements/show'
+  get 'etablissements/new'
+  get 'etablissements/create'
+  get 'etablissements/update'
+  get 'etablissements/delete'
   devise_for :users
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
