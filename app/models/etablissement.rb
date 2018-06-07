809b3eda1746429006e52d999007cd462d2290ad
ANDRIANAIVOSOA <fenitraandrianaivosoa@gmail.com>
2018-06-07 19:31:52 +0200

    role user

--- app/models/etablissement.rb
+++ app/models/etablissement.rb
@@ -4,4 +4,9 @@ class Etablissement < ApplicationRecord
 
     has_many :associate_filiere_etabs
     has_many :filiere, through: :associate_filiere_etabs
+
+    has_many :associate_niveau_etabs
+    has_many :niveau, through: :associate_niveau_etabs
+
+    has_many :articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_07_154411) do
+ActiveRecord::Schema.define(version: 2018_06_07_171820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "titre"
+    t.text "message"
+    t.string "photo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "etablissement_id"
+    t.index ["etablissement_id"], name: "index_articles_on_etablissement_id"
+  end
 
   create_table "associate_filiere_etabs", force: :cascade do |t|
     t.bigint "filiere_id"
@@ -22,6 +32,15 @@ ActiveRecord::Schema.define(version: 2018_06_07_154411) do
     t.datetime "updated_at", null: false
     t.index ["etablissement_id"], name: "index_associate_filiere_etabs_on_etablissement_id"
     t.index ["filiere_id"], name: "index_associate_filiere_etabs_on_filiere_id"
+  end
+
+  create_table "associate_niveau_etabs", force: :cascade do |t|
+    t.bigint "niveau_id"
+    t.bigint "etablissement_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["etablissement_id"], name: "index_associate_niveau_etabs_on_etablissement_id"
+    t.index ["niveau_id"], name: "index_associate_niveau_etabs_on_niveau_id"
   end
 
   create_table "associate_user_etabs", force: :cascade do |t|
@@ -53,6 +72,19 @@ ActiveRecord::Schema.define(version: 2018_06_07_154411) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "niveaus", force: :cascade do |t|
+    t.string "niveau"
+    t.integer "place"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "roles", force: :cascade do |t|
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,12 +104,18 @@ ActiveRecord::Schema.define(version: 2018_06_07_154411) do
     t.datetime "date_de_naissance"
     t.string "adresse"
     t.string "telephone"
+    t.bigint "role_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["role_id"], name: "index_users_on_role_id"
   end
 
+  add_foreign_key "articles", "etablissements"
   add_foreign_key "associate_filiere_etabs", "etablissements"
   add_foreign_key "associate_filiere_etabs", "filieres"
+  add_foreign_key "associate_niveau_etabs", "etablissements"
+  add_foreign_key "associate_niveau_etabs", "niveaus"
   add_foreign_key "associate_user_etabs", "etablissements"
   add_foreign_key "associate_user_etabs", "users"
+  add_foreign_key "users", "roles"
 end
